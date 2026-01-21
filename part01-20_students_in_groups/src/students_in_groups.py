f03979fced3ac27a41b86a178da3b03ed93522aa
Cagan <kaygincagan@gmail.com>
2026-01-21 08:53:19 +0300

    Day 2 progress

--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,3 @@
-# Write your solution here
+student = int(input("How many students on the course?"))
+size = int(input("Desired group size?"))
+print(f"Number of groups formed: {(student // size) + (student % size > 0)}")
--- part01-23_soup_or_no_soup/src/soup_or_no_soup.py
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -1,1 +1,8 @@
-# Write your solution here
+name = input("Please tell me your name")
+if name == "Jerry":
+    print(f"Next please!")
+if name != "Jerry":
+    soup = int(input("How many portions of soup?"))
+    print(f"The total cost is {5.90 * soup}")
+    print(f"Next please!")
+
